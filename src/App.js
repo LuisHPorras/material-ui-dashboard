fe5028a80ded967d0a056b439fbe05dc514f5908
luishporras <luishporras>
2021-06-17 13:25:17 +0200

    Add custom theme with ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
 import Dashboard  from "./components/Dashboard";
+import theme from "./themes/smart";
 
 function App() {
   return (  
-    <Dashboard></Dashboard>
+    <ThemeProvider theme={theme}>
+      <Dashboard></Dashboard>
+    </ThemeProvider>
   );
 }
 
---
+++ src/themes/smart.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff4054',
+    },
+    secondary: {
+      main: '#ffffff',
+    },
+  },
+});
+
+export default theme;
