ddcdadc8b34f34a4f69a52b4e32f217db872128f
luishporras <luishporras>
2021-07-01 12:33:26 +0200

    Shared an functional components

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import './App.css'
-import { ThemeProvider } from '@material-ui/core/styles'
-
-import Dashboard from './pages/Dashboard'
-import AddNonPayment from './pages/AddNonPayment/AddNonPayment'
-
-import Header from './components/shared/Header'
-import Footer from './components/shared/Footer'
-
-import theme from './themes/smart'
+import logo from './logo.svg';
+import './App.css';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Dashboard  from "./components/Dashboard";
+import theme from "./themes/smart";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <ThemeProvider theme={theme}>
-                <Header />
-                <Switch>
-                    <Route exact path="/">
-                        <Dashboard></Dashboard>
-                    </Route>
-                    <Route exact path="/add-non-payment">
-                        <AddNonPayment></AddNonPayment>
-                    </Route>
-                </Switch>
-                <Footer />
-            </ThemeProvider>
-        </BrowserRouter>
-    )
+  return (  
+    <ThemeProvider theme={theme}>
+      <Dashboard></Dashboard>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
