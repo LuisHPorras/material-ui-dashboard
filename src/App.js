bd6173b83a7dc31f0a506cce3010559b613ca780
luishporras <luishporras>
2021-07-01 12:51:50 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import { ThemeProvider } from '@material-ui/core/styles';
-import Dashboard  from "./components/Dashboard";
-import theme from "./themes/smart";
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './App.css'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import Dashboard from './pages/Dashboard'
+import AddNonPayment from './pages/AddNonPayment/AddNonPayment'
+
+import Header from './components/shared/Header'
+import Footer from './components/shared/Footer'
+
+import theme from './themes/smart'
 
 function App() {
-  return (  
-    <ThemeProvider theme={theme}>
-      <Dashboard></Dashboard>
-    </ThemeProvider>
-  );
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Dashboard></Dashboard>
+                    </Route>
+                    <Route exact path="/add-non-payment">
+                        <AddNonPayment></AddNonPayment>
+                    </Route>
+                </Switch>
+                <Footer />
+            </ThemeProvider>
+        </BrowserRouter>
+    )
 }
 
-export default App;
+export default App
